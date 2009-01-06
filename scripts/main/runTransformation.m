5f963ba4d0dde211dbd6c5331cb4de37c11ec5f4
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2009-01-06 13:20:25 +0000

    Deal with possible spaces in file names.

--- scripts/main/runTransformation.m
+++ scripts/main/runTransformation.m
@@ -2,8 +2,6 @@
 function runTransformation(srcImg, output, dofName, target, appDir)
 
 % Transform an image using a given dof and target, e.g. an anatomy.
-%
-% Previously called TransformationRun(target, source, output, dof)
 
 if (exist(output))
     disp('runTransformation:');
@@ -11,19 +9,13 @@ if (exist(output))
     return;
 end
 
-command = [appDir '/transformation ' srcImg ' ' output ' -dofin ' dofName ' -target ' target ' -bspline' ];
+command = [appDir '/transformation '];
+command = [command ' "' srcImg '"'];
+command = [command ' "' output '"'];
+command = [command ' -dofin "' dofName '"'];
+command = [command ' -target "'  target '"'];
+command = [command ' -bspline'];
+
 disp(command);
 system(command);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% OLD CODE:
-% 
-% if (isempty(dir(output))==0)
-%     disp([' exists : ' output ' ... ' ]);
-%     return;
-% end
-% 
-% command = ['transformation' ' ' source ' ' output ' ' '-dofin' ' ' dof ' ' '-target' ' ' target ' '  '-bspline' ];
-% command
-% [s, w] = dos(command, '-echo');
-% return
