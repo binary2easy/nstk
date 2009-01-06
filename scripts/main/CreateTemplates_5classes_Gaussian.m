38a98b7e1bfff18a45e777fc88d64f6b72ae28a2
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2009-01-06 22:17:25 +0000

    Replaced old slow code and generally tidied.

--- scripts/main/CreateTemplates_5classes_Gaussian.m
+++ scripts/main/CreateTemplates_5classes_Gaussian.m
@@ -1,92 +1,52 @@
 
-function [csfT, wmT1, wmT2, cortexT, outlierT] = CreateTemplates_5classes_Gaussian(header, csfSeg, wmSeg1, wmSeg2, cortexSeg, outlierSeg, ...
-                    sigmaCSF, sigmaWM1, sigmaWM2, sigmaCortex, sigmaOutlier,...
-                    halfwidthCsf, halfwidthWm1, halfwidthWm2, halfwidthCortex, halfwidthOutlier)
+function [csfT, wmT1, wmT2, gmT, outlierT] = CreateTemplates_5classes_Gaussian(header, ...
+  csfSeg, wmSeg1, wmSeg2, gmSeg, outlierSeg, ...
+  sigmaCSF, sigmaWM1, sigmaWM2, sigmaCortex, sigmaOutlier)
                 
-disp('create templates ...');
-% create probability templates
+% Create probability templates by blurring hard label maps.
 
-csfT = single(csfSeg);
-G_csfT = gaussianFilter(csfT, header, sigmaCSF, sigmaCSF, sigmaCSF, halfwidthCsf, 'Real');
-clear csfSeg
+disp('Create templates by blurring k-means result (5 classes).');
 
-cortexT = single(cortexSeg);
-G_cortexT = gaussianFilter(cortexT, header, sigmaCortex, sigmaCortex, sigmaCortex, halfwidthCortex, 'Real');
-clear cortexSeg
-
-wmT1 = single(wmSeg1);
-G_wmT1 = gaussianFilter(wmT1, header, sigmaWM1, sigmaWM1, sigmaWM1, halfwidthWm1, 'Real');
-clear wmSeg1
-
-wmT2 = single(wmSeg2);
-G_wmT2 = gaussianFilter(wmT2, header, sigmaWM2, sigmaWM2, sigmaWM2, halfwidthWm2, 'Real');
-clear wmSeg2
-
+csfT     = single(csfSeg);
+gmT      = single(gmSeg);
+wmT1     = single(wmSeg1);
+wmT2     = single(wmSeg2);
 outlierT = single(outlierSeg);
-G_outlierT = gaussianFilter(outlierT, header, sigmaOutlier, sigmaOutlier, sigmaOutlier, halfwidthOutlier, 'Real');
-clear outlierSeg
 
-% clear csfSeg wmSeg1 wmSeg2 cortexSeg outlierSeg
+G_csfT     = gaussianFilter(csfT, header, sigmaCSF, sigmaCSF, sigmaCSF, 'Real');
+G_gmT      = gaussianFilter(gmT, header, sigmaCortex, sigmaCortex, sigmaCortex, 'Real');
+G_wmT1     = gaussianFilter(wmT1, header, sigmaWM1, sigmaWM1, sigmaWM1, 'Real');
+G_wmT2     = gaussianFilter(wmT2, header, sigmaWM2, sigmaWM2, sigmaWM2, 'Real');
+G_outlierT = gaussianFilter(outlierT, header, sigmaOutlier, sigmaOutlier, sigmaOutlier, 'Real');
 
-
-csfflag = 0;
-wm1flag = 0;
-wm2flag = 0;
-cortexflag = 0;
-outlierflag = 0;
 
 minP = 0.02;
 
-for k = 1:header.zsize
-    for j = 1:header.ysize
-        for i = 1:header.xsize
-            
-            p_csf = G_csfT(j, i, k);
-            p_wm1 = G_wmT1(j, i, k);
-            p_wm2 = G_wmT2(j, i, k);
-            p_cortex = G_cortexT(j, i, k);
-            p_outlier = G_outlierT(j, i, k);
+csfPresent     = (G_csfT > minP);
+gmPresent      = (G_gmT > minP);
+wm1Present     = (G_wmT1 > minP);
+wm2Present     = (G_wmT2 > minP);
+outlierPresent = (G_outlierT > minP);
 
-            if ( G_csfT(j, i, k)>minP )
-                csfflag = 1;
-            end
-            
-            if ( G_wmT1(j, i, k)>minP )
-                wm1flag = 1;
-            end
-            
-            if ( G_wmT2(j, i, k)>minP )
-                wm2flag = 1;
-            end   
-            
-            if ( G_cortexT(j, i, k)>minP )
-                cortexflag = 1;
-            end
-            
-            if ( G_outlierT(j, i, k)>minP )
-                outlierflag = 1;
-            end
-            
-            if ( csfflag + wm1flag + wm2flag + cortexflag + outlierflag == 0 )
-                continue;
-            end
-            
-            csfT(j, i, k) = p_csf/(p_csf+p_wm1+p_wm2+p_cortex+p_outlier);
-            wmT1(j, i, k) = p_wm1/(p_csf+p_wm1+p_wm2+p_cortex+p_outlier);
-            wmT2(j, i, k) = p_wm2/(p_csf+p_wm1+p_wm2+p_cortex+p_outlier);
-            cortexT(j, i, k) = p_cortex/(p_csf+p_wm1+p_wm2+p_cortex+p_outlier);
-            outlierT(j, i, k) = p_outlier/(p_csf+p_wm1+p_wm2+p_cortex+p_outlier);
-            
-%             csfT(j, i, k) = 0.33;
-%             wmT(j, i, k) = 0.33;
-%             cortexT(j, i, k) = 0.33;
-            
-            csfflag = 0;
-            wm1flag = 0;
-            wm2flag = 0;
-            cortexflag = 0;
-            outlierflag = 0;
-        end
-    end
-end
-return;
+anyPresent = gmPresent + wm1Present + wm2Present + csfPresent + outlierPresent;
+
+nonePresent = anyPresent < 1;
+noneInds    = find(nonePresent);
+
+G_sum = G_wmT1 + G_wmT2 + G_gmT + G_csfT + G_outlierT ;
+
+csfT     = G_csfT ./ G_sum;
+gmT      = G_gmT ./ G_sum;
+wmT1     = G_wmT1 ./ G_sum;
+wmT2     = G_wmT2 ./ G_sum;
+outlierT = G_outlierT ./ G_sum;
+
+temp = zeros(size(noneInds));
+
+gmT(noneInds)      = temp;
+wmT1(noneInds)      = temp;
+wmT2(noneInds)      = temp;
+csfT(noneInds)     = temp;
+outlierT(noneInds) = temp;
+
+return
