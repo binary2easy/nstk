839c8ab4b1e774dbc920b95b4741913932edd1f5
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2009-01-06 13:19:53 +0000

    Deal with possible spaces in file names.

--- scripts/main/runTransformationBinary.m
+++ scripts/main/runTransformationBinary.m
@@ -12,7 +12,13 @@ if (exist(output))
     return;
 end
 
-command = [appDir '/transformation ' srcImg ' ' output ' -dofin ' dofName ' -target ' target ' -linear' ];
+command = [appDir '/transformation '];
+command = [command ' "' srcImg '"'];
+command = [command ' "' output '"'];
+command = [command ' -dofin "' dofName '"'];
+command = [command ' -target "'  target '"'];
+command = [command ' -bspline'];
+
 disp(command);
 [s, w] = system(command);
 
@@ -28,35 +34,4 @@ pp = imclose(data, se);
 pp(find(pp>0)) = 1;
 saveAnalyze(uint32(pp), header, output, 'Grey');
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% OLD CODE:
 
-% Alternative to using matlab toolbox imclose function (image proc
-% toolbox).
-
-% command = ['threshold ' output ' ' output ' 0'];
-% disp(command);
-% system(command);
-% 
-% command = ['closing ' output ' ' output];
-% disp(command);
-% system(command);
-
-% Hui's original script:
-
-% if (isempty(dir(output))==0)
-%     disp([' exists : ' output ' ... ' ]);
-%     return;
-% end
-% 
-% command = ['transformation' ' ' source ' ' output ' ' '-dofin' ' ' dof ' ' '-target' ' ' target ' '  '-bspline' ];
-% command
-% [s, w] = dos(command, '-echo');
-% 
-% [data, header] = loadAnalyze(output, 'Grey');
-% se = strel('ball', 3, 3, 0);
-% pp = imclose(data, se);
-% pp(find(pp>0)) = 1;
-% SaveAnalyze(uint32(pp), header, output, 'Grey');
-% 
-% return
