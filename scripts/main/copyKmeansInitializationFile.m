37bb79c957ff6328575119d0b3831ff91c02562d
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2009-01-06 22:13:33 +0000

    Deal with case when k-means centres file is not provided.

--- scripts/main/copyKmeansInitializationFile.m
+++ scripts/main/copyKmeansInitializationFile.m
@@ -3,8 +3,6 @@ function copyKmeansInitializationFile(rootDir, fourClasses_flag, fiveClasses_fla
 saveDir = pwd;
 cd(rootDir);
 
-[subdirs, num] = findAllDirectory(rootDir);
-
 kmeansSubDir = 'kmeans';
 
 name_4class = 'Kmeans_InitialCentres_4classes.txt';
@@ -13,8 +11,11 @@ name_5class = 'Kmeans_InitialCentres_5classes.txt';
 file_4class = fullfile(rootDir, name_4class);
 file_5class = fullfile(rootDir, name_5class);
 
-% ============================================================== %
-% resolution = [];
+if (isempty(dir(file_4class)) && isempty(dir(file_5class)))
+  return
+end
+
+[subdirs, num] = findAllDirectory(rootDir);
 
 for i = 1:num
     
@@ -28,12 +29,12 @@ for i = 1:num
     KmeansDir = fullfile(currentDir, kmeansSubDir);
     
     dstfile = fullfile(KmeansDir, name_4class);
-    if ( (isempty(dir(dstfile))==1)  && fourClasses_flag )
+    if ( isempty(dir(dstfile))  && fourClasses_flag && ~isempty(dir(file_4class)))
         copyfile(file_4class, dstfile); 
     end
 
     dstfile = fullfile(KmeansDir, name_5class);
-    if ( (isempty(dir(dstfile))==1) && fiveClasses_flag)
+    if ( isempty(dir(dstfile)) && fiveClasses_flag && ~isempty(dir(file_5class)))
         copyfile(file_5class, dstfile);
     end
 end
