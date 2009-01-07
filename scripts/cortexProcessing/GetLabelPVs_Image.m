fff0f72fe1ac5b2eb89c7132177eac1cf1bb6f57
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2009-01-07 14:27:34 +0000

    Cosmetic.

--- scripts/cortexProcessing/GetLabelPVs_Image.m
+++ scripts/cortexProcessing/GetLabelPVs_Image.m
@@ -1,13 +1,10 @@
 
 function LabelPVs_Image = GetLabelPVs_Image(mix)
-% transform posterior as image file
 
 xsize = mix.header.xsize;
 ysize = mix.header.ysize;
 zsize = mix.header.zsize;
 
-% PA CHANGING XY SUBSCRIPTS.
-% LabelPVs_Image = zeros([ysize xsize zsize], 'uint32');
 LabelPVs_Image = zeros([xsize ysize zsize], 'uint32');
 
 if ( isempty(mix.LabelPVs) == 0 )
