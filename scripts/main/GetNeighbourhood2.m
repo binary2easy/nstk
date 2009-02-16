dd8443fa917bc4828c4fa864b31b40b6c196d644
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2009-02-16 22:24:54 +0000

    Tidying.

--- scripts/main/GetNeighbourhood2.m
+++ scripts/main/GetNeighbourhood2.m
@@ -1,7 +1,7 @@
 
 function [neighbourhood, neighbors] = GetNeighbourhood2(data, header, x, y, z, N)
-% get the N-neighbourhood of (x, y, z) from a 3D image
-% the padding value is -inf
+% Get the N-neighbourhood of (x, y, z) from a 3D image. Padding value is
+% -inf. 
 
 xsize = header.xsize;
 ysize = header.ysize;
@@ -10,28 +10,31 @@ zsize = header.zsize;
 halfN = floor(N/2);
 
 neighbourhood = zeros(N, N, N);
-neighbors = zeros(N*N*N, 1)-1;
+neighbors = zeros(N*N*N, 1) - 1;
 
 index = 0;
-for k = -halfN:halfN
-    for j = -halfN:halfN
-        for i = -halfN:halfN
 
-            px = x + i; 
-            py = y + j; 
-            pz = z + k;
-            index = index + 1;
+minCorner = ones(1, 3);
+maxCorner = [xsize ysize zsize];
+
+for k = -halfN:halfN
+  for j = -halfN:halfN
+    for i = -halfN:halfN
+
+      px = x + i; 
+      py = y + j;
+      pz = z + k;
+      index = index + 1;
             
-            if ( pointInROI([px py pz], [1, 1, 1], [xsize ysize zsize]) == true )
-                % PA CHANGING XY SUBSCRIPTS.
-%                 neighbourhood(i + halfN + 1, j + halfN + 1, k + halfN + 1) = data(py, px, pz);
-%                 neighbors(index) = data(py, px, pz);
-                neighbourhood(i + halfN + 1, j + halfN + 1, k + halfN + 1) = data(px, py, pz);
-                neighbors(index) = data(px, py, pz);
-            else
-                neighbourhood(i + halfN + 1, j + halfN + 1, k + halfN + 1) = -inf;
-            end
-        end
+      if (pointInROI([px py pz], minCorner, maxCorner))
+        neighbourhood(i + halfN + 1, j + halfN + 1, k + halfN + 1) = data(px, py, pz);
+        neighbors(index) = data(px, py, pz);
+      else
+        neighbourhood(i + halfN + 1, j + halfN + 1, k + halfN + 1) = -inf;
+      end
+      
     end
+  end
 end
+
 return
