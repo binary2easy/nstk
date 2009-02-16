c8fc1c8186bebd7ca4ce0ac0afcad0f40088df9c
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2009-02-16 22:20:00 +0000

    Tidying and formatting.

--- scripts/cortexProcessing/GetPVs_WM2CSF.m
+++ scripts/cortexProcessing/GetPVs_WM2CSF.m
@@ -1,106 +1,114 @@
-
 function indexInPrior = GetPVs_WM2CSF(mix, suspectWMPVs, segResult, LabelSeg,...
-    csflabel, cortexlabel, pvlabel, VolumeThres);
-% check the first-order neighbor of suspectWMPVs
-% if the most neighboring voxels are gm, this volume is assigned as the incorrectly classified csf
-% the prior of wm should decrease and the csf prior should increase
+    csflabel, cortexlabel, pvlabel, VolumeThres)
 
-[l,num] = bwlabeln(suspectWMPVs,26);
-sizeL = size(l);            
+% Check the first-order neighbor of suspect WM PVs.
+% If most of the neighbouring voxels are GM, this volume is assigned as
+% incorrectly classified CSF the prior of WM should decrease and the CSF
+% prior should increase.
+
+% Connected components in WM PVs image
+[components, numComps] = bwlabeln(suspectWMPVs, 26);
+sizeComps = size(components);
 
 xsize = mix.header.xsize;
 ysize = mix.header.ysize;
 zsize = mix.header.zsize;
 
 ndata = size(mix.priors, 1);
-disp(['Total region: ' num2str(num)]);
 
-tic
-% indexInPrior = GetPVs_WM2CSF_MEX(uint32(l), num, segResult, LabelSeg, mix.indexVolume, ...
-%     csflabel, cortexlabel, pvlabel, VolumeThres, xsize, ysize, zsize, mix.neighborNum, ndata);
+disp(['Total region: ' num2str(numComps)]);
 
-seglabels = [];
+% tic
+
+seglabels    = [];
 indexInPrior = [];
-offsets = mix.offset;
+
+offsets    = mix.offset;
 numOffsets = size(offsets, 1);
-disp(['Total region: ' num2str(num)]);
-for ss = 1:num
-    if ( mod(ss,50) == 0 )
-        disp(['current region: ' num2str(ss)]);
-    end
-    [i, j, k] = ind2sub(sizeL, find(l==ss)); % row, col, depth
-    numRegion = length(i);
-    seglabels = [];
-    for tt = 1:numRegion
 
-        % PA CHANGING XY SUBSCRIPTS.
-%         y = i(tt);
-%         x = j(tt);
-%         z = k(tt);
-        x = i(tt);
-        y = j(tt);
-        z = k(tt);
-        index = 0;
-%         if (LabelSeg(y, x, z) ~= pvlabel)
-        if (LabelSeg(x, y, z) ~= pvlabel)
+disp(['Total region: ' num2str(numComps)]);
+
+for compIndex = 1:numComps
+  
+  if ( mod(compIndex, 50) == 0 )
+    disp(['current region: ' num2str(compIndex)]);
+  end
+  
+  [i, j, k] = ind2sub(sizeComps, find(components == compIndex)); % row, col, depth
+  
+  regionVolume = length(i);
+  seglabels = [];
+  
+  % Loop over current component.
+  for tt = 1:regionVolume
+    
+    x = i(tt);
+    y = j(tt);
+    z = k(tt);
+    index = 0;
+
+    if (LabelSeg(x, y, z) ~= pvlabel)
+
+      neighbors = zeros(numOffsets, 1);
             
-            %neighbors = GetNeighbourhood2_6neighbors(suspectWMPVs, mix.header, mix.offsets, j(tt), i(tt), k(tt))
-            neighbors = zeros(numOffsets, 1);
-            
-            for pp = 1:numOffsets
-                px = x + offsets(pp, 1);
-                py = y + offsets(pp, 2);
-                pz = z + offsets(pp, 3);                
-                if ( pointInROI([px py pz], [1, 1, 1], [xsize ysize zsize]) == true )
-                    %PA CHANGING XY SUBSCRIPTS
-                    %if (LabelSeg(py, px, pz) == pvlabel)
-                    if (LabelSeg(px, py, pz) == pvlabel)
-                        continue;
-                    end
-                    %PA CHANGING XY SUBSCRIPTS
-                    %if (l(py, px, pz) > 0)
-                    if (l(px, py, pz) > 0)
-                        continue;
-                    end
-                    %PA CHANGING XY SUBSCRIPTS
-                    %neighbors(index+1) = segResult(py, px, pz);
-                    neighbors(index+1) = segResult(px, py, pz);
-                    index = index + 1;
-                end
-            end
+      for pp = 1:numOffsets
+        px = x + offsets(pp, 1);
+        py = y + offsets(pp, 2);
+        pz = z + offsets(pp, 3);
+
+        if ( pointInROI([px py pz], [1, 1, 1], [xsize ysize zsize]) == true )
+
+          if (LabelSeg(px, py, pz) == pvlabel)
+            continue;
+          end
+                  
+          if (components(px, py, pz) > 0)
+            continue;
+          end
+                  
+          neighbors(index+1) = segResult(px, py, pz);
+          index = index + 1;
         end
-        if ( index > 0 )
-            seglabels = [seglabels; neighbors(1:index)];
-        end
+      end
     end
-    if ( isempty(seglabels) == 0 )
+    
+    if ( index > 0 )
+      seglabels = [seglabels; neighbors(1:index)];
+    end
+    
+  end % Loop over current component.
+  
+  if ( isempty(seglabels) == 0 )
        
-        recordflag = false;
+    recordflag = false;
         
-        numGM = length(find(seglabels==cortexlabel));
-        numCSF = length(find(seglabels==csflabel));
-        numSegLabels = length(seglabels);
+    numGM = length(find(seglabels==cortexlabel));
+    numCSF = length(find(seglabels==csflabel));
+    numSegLabels = length(seglabels);
         
-        if ( numGM/numSegLabels > VolumeThres )
-            recordflag = true;
-        end % around by GM
+    if ( numGM/numSegLabels > VolumeThres )
+      recordflag = true;
+    end % around by GM
         
-        if ( numCSF/numSegLabels > VolumeThres )
-            recordflag = true;
-        end % around by csf
+    if ( numCSF/numSegLabels > VolumeThres )
+      recordflag = true;
+    end % around by csf
         
-        if ( (numCSF+numGM)/numSegLabels > 0.9 )
-            recordflag = true;
-        end % around by csf and GM
+    if ( (numCSF+numGM)/numSegLabels > 0.9 )
+      recordflag = true;
+    end % around by csf and GM
 
-        if ( recordflag )
-            currentIndexInPrior = zeros(numRegion, 1);
-            for tt = 1:numRegion
-                currentIndexInPrior(tt) = mix.indexVolume(i(tt), j(tt), k(tt));
-            end   
-            indexInPrior = [indexInPrior; currentIndexInPrior];
-        end
+    if ( recordflag )
+      currentIndexInPrior = zeros(regionVolume, 1);
+  
+      for tt = 1:regionVolume
+        currentIndexInPrior(tt) = mix.indexVolume(i(tt), j(tt), k(tt));
+      end
+      indexInPrior = [indexInPrior; currentIndexInPrior];
     end
+  end
 end
-toc
+
+% toc
+
 return;
