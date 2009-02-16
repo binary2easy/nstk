0e835c2d16dc37ebc0310e5b5bf8e25f5dcaa584
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2009-02-16 22:26:05 +0000

    Tidying and insured NIFTI header dimensions are updated.

--- scripts/main/addExtraWidth.m
+++ scripts/main/addExtraWidth.m
@@ -1,23 +1,36 @@
 
-function [ROI, headerROI] = AddExtraWidth(data, header, extraWidth)
-% add some '0' at each dimension to prevent the errors
+function [bigData, bigHeader] = addExtraWidth(data, header, extraWidth)
+
+% Add some zeros at each boundary to prevent the errors
 
 xsize = header.xsize;
 ysize = header.ysize;
 zsize = header.zsize;
 
-new_xsize = xsize + 2*extraWidth;
-new_ysize = ysize + 2*extraWidth;
-new_zsize = zsize + 2*extraWidth;
+new_xsize = xsize + 2 * extraWidth;
+new_ysize = ysize + 2 * extraWidth;
+new_zsize = zsize + 2 * extraWidth;
 
-ROI = zeros([new_ysize new_xsize new_zsize]);
+bigData = zeros([new_xsize new_ysize new_zsize]);
 
-ROI(extraWidth+1:ysize+extraWidth, extraWidth+1:xsize+extraWidth, extraWidth+1:zsize+extraWidth) = data;
+startRange = extraWidth + 1;
 
-headerROI = header;
-headerROI.xsize = new_xsize;
-headerROI.ysize = new_ysize;
-headerROI.zsize = new_zsize;
+xrange = startRange:xsize+extraWidth;
+yrange = startRange:ysize+extraWidth;
+zrange = startRange:zsize+extraWidth;
+
+bigData(xrange, yrange, zrange) = data;
+
+bigHeader = header;
+
+bigHeader.xsize = new_xsize;
+bigHeader.ysize = new_ysize;
+bigHeader.zsize = new_zsize;
+
+newdims = [new_xsize new_ysize new_zsize];
+
+bigHeader.nii.hdr.dime.dim(2:4) = newdims;
+bigHeader.nii.original.hdr.dime.dim(2:4) = newdims;
 
 return;
 
