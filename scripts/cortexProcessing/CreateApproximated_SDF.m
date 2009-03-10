6f87c2814cd6ae9151e2386f572afb3603eebdd8
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2009-03-10 20:25:27 +0000

    Continuing edit.

--- scripts/cortexProcessing/CreateApproximated_SDF.m
+++ scripts/cortexProcessing/CreateApproximated_SDF.m
@@ -1,16 +1,18 @@
 
-function SDF = CreateApproximated_SDF(data, header)
-% create an approximated signed distance function for a 3D bianry volume
+function SDF = createApproximatSDF(data, header)
+
+% Create an approximated signed distance function for a 3D binary volume
 % the data should be a volume with isotropic resolution, so that the image
 % coordinates equal to the world coordinate
-% the 6-connected neighborhood is used for both inter and outer
-% surfaces
-% the distance of surface voxels is assigned to be +/- 0.5 (pixel
-% coordinate)
-% the Euler distances of other points are computed using the k-d tree
-% locator
 
-disp('preforming the distance transform ... ');
+% The 6-connected neighborhood is used for both inter and outer
+% surfaces.
+% The distance of surface voxels is assigned to be +/- 0.5 (pixel
+% coordinate).
+% The Euler distances of other points are computed using the k-d tree
+% locator.
+
+disp('Performing the distance transform ... ');
 
 offsets = [-1 0 0; 1 0 0; 0 -1 0; 0 1 0; 0 0 -1; 0 0 1];
 
@@ -20,42 +22,54 @@ zsize = header.zsize;
 
 SDF = zeros(size(data), 'double');
 
-points = zeros(xsize*ysize*floor(zsize/2), 3); % store the points on the inter/outer surface
+% Heuristically make enough room in the array for surface points based on
+% half the number of voxels in the image.
+% store the points on the inter/outer surface
+points = zeros(xsize * ysize * floor(zsize/2), 3); 
 
-% this loop will get all surface points, so it is safe not to detect all
-% boundary points
+% This loop will get all surface points, so it is safe not to detect all
+% boundary points.
+
 index = 0;
 for k = 2:zsize-1
-    for j = 2:ysize-1
-        for i = 2:xsize-1
+  for j = 2:ysize-1
+    for i = 2:xsize-1
             
-            neighbors = GetNeighbourhood2_6neighbors(data, header, offsets, i, j, k);
+      neighbors = getNeighbourhood2_6neighbors(data, header, offsets, i, j, k);
             
-            hasOne = find(neighbors==1);
-            hasZero = find(neighbors==0);
+      hasOne  = find(neighbors == 1);
+      hasZero = find(neighbors == 0);
             
-            if ( (length(hasOne)>0) & (length(hasZero)>0) )
-               if ( data(j, i, k) == 1 )
-                   SDF(j, i, k ) = -0.5;
-               else
-                   SDF(j, i, k ) = 0.5;
-               end
-               index = index + 1;
-               points(index, :) = [i, j, k];
-            end
+      if ( (length(hasOne) > 0) && (length(hasZero) > 0) )
+
+        if ( data(i, j, k) == 1 )
+          SDF(i, j, k ) = -0.5;
+        else
+          SDF(i, j, k ) = 0.5;
         end
+
+        index = index + 1;
+        points(index, :) = [i, j, k];
+      end
     end
+  end
 end
+
+% Truncate points array, not all voxels are near the surface 
 points = points(1:index, :);
 
-% internal points
+% Internal points
 internalInd = find(data==1);
 numOfInternal = length(internalInd);
-[j, i, k] = ind2sub(size(data), internalInd);
+
+[i, j, k] = ind2sub(size(data), internalInd);
+
 points_Internal = [i, j, k];
+
 clear internalInd i j k
 
 [minDist, nearestPoints] = GetNearestPoints_VTK(points_Internal, points);
+
 for tt = 1:numOfInternal
     i = points_Internal(tt, 1);
     j = points_Internal(tt, 2);
