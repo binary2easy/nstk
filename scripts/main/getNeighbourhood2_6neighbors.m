e8ac8cf45d12a7d2d01a919672055356980bd6fb
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2009-03-10 20:13:15 +0000

    Tidying up. Swapping x-y indices.

--- scripts/main/getNeighbourhood2_6neighbors.m
+++ scripts/main/getNeighbourhood2_6neighbors.m
@@ -1,20 +1,26 @@
+function neighbours = GetNeighbourhood2_6neighbors(data, header, offsets, x, y, z)
 
-function neighbors = GetNeighbourhood2_6neighbors(data, header, offsets, x, y, z)
-% get the 6-neighbourhood of (x, y, z) from a 3D image
+% Get the 6-neighbourhood of (x, y, z) from a 3D image
 % the padding value is -inf
 
 xsize = header.xsize;
 ysize = header.ysize;
 zsize = header.zsize;
-neighbors = zeros(1, 6);
-index = 0;
+
+neighbours = zeros(1, 6);
+
 for pp = 1:6
-    px = x + offsets(pp, 1); py = y + offsets(pp, 2); pz = z + offsets(pp, 3);
-    index = index + 1;
-    if ( pointInROI([px py pz], [1, 1, 1], [xsize ysize zsize]) == true )
-        neighbors(pp) = data(py, px, pz);
-    else
-        neighbors(pp) = -inf;
-    end
+    
+  px = x + offsets(pp, 1);
+  py = y + offsets(pp, 2);
+  pz = z + offsets(pp, 3);
+
+  if pointInROI([px py pz], [1, 1, 1], [xsize ysize zsize])
+    neighbours(pp) = data(px, py, pz);
+  else
+    neighbours(pp) = -inf;
+  end
+  
 end
+
 return
