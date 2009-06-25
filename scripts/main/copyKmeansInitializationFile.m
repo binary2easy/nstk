a03074b2c9dd64c366a0efbaca44dc2f5dd1a8db
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2009-06-25 22:22:00 +0000

    Removed 4 and 5 class flags.  Copy any files that exist containing k-means centres.

--- scripts/main/copyKmeansInitializationFile.m
+++ scripts/main/copyKmeansInitializationFile.m
@@ -1,4 +1,4 @@
-function copyKmeansInitializationFile(rootDir, fourClasses_flag, fiveClasses_flag)
+function copyKmeansInitializationFile(rootDir)
 
 saveDir = pwd;
 cd(rootDir);
@@ -11,7 +11,7 @@ name_5class = 'Kmeans_InitialCentres_5classes.txt';
 file_4class = fullfile(rootDir, name_4class);
 file_5class = fullfile(rootDir, name_5class);
 
-if (isempty(dir(file_4class)) && isempty(dir(file_5class)))
+if (~exist(file_4class, 'file') && ~exist(file_5class, 'file'))
   return
 end
 
@@ -29,12 +29,12 @@ for i = 1:num
     KmeansDir = fullfile(currentDir, kmeansSubDir);
     
     dstfile = fullfile(KmeansDir, name_4class);
-    if ( isempty(dir(dstfile))  && fourClasses_flag && ~isempty(dir(file_4class)))
+    if ( isempty(dir(dstfile)) && ~isempty(dir(file_4class)))
         copyfile(file_4class, dstfile); 
     end
 
     dstfile = fullfile(KmeansDir, name_5class);
-    if ( isempty(dir(dstfile)) && fiveClasses_flag && ~isempty(dir(file_5class)))
+    if ( isempty(dir(dstfile)) && ~isempty(dir(file_5class)))
         copyfile(file_5class, dstfile);
     end
 end
