36d6bc5dfd7325490634e11f5613a9b3e2fc4514
Paul Aljabar <paul.aljabar@imperial.ac.uk>
2010-04-10 17:58:34 +0000

    Tidying, removed redundant code. Added optional argument if a single scan folder is to be processed.

--- scripts/main/AllinOne_Reconstruction.m
+++ scripts/main/AllinOne_Reconstruction.m
@@ -1,42 +1,23 @@
-function AllinOne_Reconstruction(rootDir, fourClasses_flag, fiveClasses_flag, appDir)
-%function AllinOne_Reconstruction(rootDir, list, fourClasses_flag, fiveClasses_flag, appDir)
+function AllinOne_Reconstruction(rootDir, fourClasses_flag, fiveClasses_flag, ...
+  appDir, subfolder)
 
 disp('----------------------------------------------------');
 disp('AllinOne_Reconstruction');
 
-% Try and avoid this step.
-% copyFiles_cortexReconstruction; 
-
-
-cortex_reconstruction = 'cortex_reconstruction';
-[subdirs, num] = findAllDirectory(rootDir);
-% num = length(list);
-% subdirs = list;
-
-
-if ( fourClasses_flag )
-  csf_seg_filename = 'csf_seg_4classes.hdr';
-  wm_seg_filename = 'wm_seg_4classes.hdr';
-  
-  post_wm_Real_filename = 'post_wm_Real.hdr';
-  post_gm_Real_filename = 'post_gm_Real.hdr';
-  post_csf_Real_filename = 'post_csf_Real.hdr';
-  Brainfilename = 'withStemBrain_N3.hdr';
-end
-
-if ( fiveClasses_flag )
-  csf_seg_filename = 'csf_seg.hdr';
-  wm_seg_filename = 'wm_seg.hdr';
-  
-  post_wm_Real_filename = 'post_wm_Real.hdr';
-  post_gm_Real_filename = 'post_gm_Real.hdr';
-  post_csf_Real_filename = 'post_csf_Real.hdr';
-  Brainfilename = 'withStemBrain_N3.hdr';
+if (nargin == 4)
+  [subdirs, num] = findAllDirectory(rootDir);
+elseif (nargin == 5)
+  % Single subfolder to process.
+  subdirs = {subfolder};
+  num = 1;  
+else
+  error('AllinOne_Reconstruction : called with wrong number of arguments.');  
 end
 
 for i=1:num
     
   subjDir = fullfile(rootDir, subdirs{i});
+  disp(['AllinOne_Reconstruction : ' subjDir ]);
 
   if (fourClasses_flag)
     Cortex_Reconstruction_WholePipeline_AllRun(subjDir, 4, appDir);
